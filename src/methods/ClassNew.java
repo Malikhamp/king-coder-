b202ef1f9429a52b76d4477e26358a8a3623e97f
Malik <ginoswint@gmail.com>
2019-10-31 14:42:59 -0700

    Made another comment

--- src/methods/ClassNew.java
+++ src/methods/ClassNew.java
@@ -9,7 +9,7 @@ public class ClassNew {
 		//stream of int from 1-19 -> println
 		IntStream.range(1, 20).forEach(System.out::println);
 		
-		List<Integer> integers = IntStream // Listing Integer
+		List<Integer> integers = IntStream // Listing Integer & IntStream
 				.range(1,100)
 				.boxed()
 				.collect(Collectors.toCollection(ArrayList::new));
